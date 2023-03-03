dcd1a90e2eb6d99e430182b10f0bf00618cc656f
lynlen <m.daffa342@gmail.com>
2023-03-03 14:50:29 +0700

    membuat logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,4 +45,12 @@ class AuthController extends Controller
         Session::flash('message', 'Login Invalid');
         return redirect('/login');
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 
@@ -20,9 +21,15 @@ Route::get('/', function () {
     return view('welcome');
 })->middleware('auth');
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'authenticating']);
-Route::get('register', [AuthController::class, 'register']);
+Route::middleware('only_guest')->group(function(){
+    Route::get('login', [AuthController::class, 'login'])->name('login');
+    Route::post('login', [AuthController::class, 'authenticating']);
+    Route::get('register', [AuthController::class, 'register']);
+});
 
-Route::get('dashboard', [DashboardController::class, 'index']);
-Route::get('profile', [UserController::class, 'profile']);
+Route::middleware('auth')->group(function(){
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['only_admin']);
+    Route::get('profile', [UserController::class, 'profile'])->middleware(['only_client']);
+    Route::get('books', [BookController::class, 'index']);
+});
