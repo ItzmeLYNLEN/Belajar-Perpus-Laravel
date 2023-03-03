ecd4f56152a16935e22eaf426511bffa16e59f47
lynlen <m.daffa342@gmail.com>
2023-03-03 16:34:46 +0700

    register & create data user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
@@ -26,18 +28,22 @@ class AuthController extends Controller
         ]);
 
         if (Auth::attempt($credentials)) {
-            if (Auth::user()->status != 'active'){
+            if (Auth::user()->status != 'active') {
+                Auth::logout();
+                $request->session()->invalidate();
+                $request->session()->regenerateToken();
+
                 Session::flash('status', 'Failed');
                 Session::flash('message', 'Your account is not active yet. Please contact admin!');
                 return redirect('/login');
             }
 
-            // $request->session()->regenerate();
-            if(Auth::user()->role_id == 1){
+            $request->session()->regenerate();
+            if (Auth::user()->role_id == 1) {
                 return redirect('dashboard');
             }
 
-            if(Auth::user()->role_id == 2){
+            if (Auth::user()->role_id == 2) {
                 return redirect('profile');
             }
         }
@@ -52,5 +58,22 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('login');
+    }
+
+    public function registerprocess(Request $request)
+    {
+        $validate = $request->validate([
+            'username' => 'required|unique:users|max:255',
+            'password' => 'required|max:255',
+            'phone' => 'max:255',
+            'address' => 'required',
+        ]);
+
+        $request['password'] = Hash::make($request->password);
+        $user = User::create($request->all());
+
+        Session::flash('status', 'success');
+        Session::flash('message', 'Register Success. Wait admin for approval');
+        return redirect('register');
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perpus | Regiter</title>
+    <title>Perpus | Register</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 <style>
@@ -14,7 +14,7 @@
         box-sizing: border-box;
     }
 
-    .login-box {
+    .register-box {
         padding: 20px;
         width: 450px;
     }
@@ -23,18 +23,42 @@
 <body>
 
     <div class="main d-flex justify-content-center align-items-center">
-        <div class="login-box col-lg-5">
+        <div class="register-box col-lg-5">
             <div class="card shadow-sm">
                 <div class="card-body">
                     <h5 class="fw-600 mb-4 text-center">Register</h5>
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
+                    @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                    @endif
                     <form action="" method="post">
-                        <div class="mb-3">
+                        @csrf
+                        <div class="mb-2">
                             <label for="username" class="form-label">Username</label>
                             <input type="text" class="form-control form-control-lg" id="username" name="username" placeholder="Zee punya daoa (contoh)">
                         </div>
-                        <div class="mb-3">
+                        <div class="mb-2">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" class="form-control form-control-lg" id="password" name="password">
+                        </div>
+                        <div class="mb-2">
+                            <label for="phone" class="form-label">Phone</label>
+                            <input type="text" class="form-control form-control-lg" id="phone" name="phone">
+                        </div>
+                        <div class="mb-2">
+                            <label for="address" class="form-label">Address</label>
+                            <textarea name="address" id="address" class="form-control" rows="2" required></textarea>
                         </div>
                         <div>
                             <button class="btn btn-primary btn-lg form-control">Sign Up</button>
